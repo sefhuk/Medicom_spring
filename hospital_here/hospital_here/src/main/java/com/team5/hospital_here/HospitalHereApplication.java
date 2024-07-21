c2c2600f3c77e6b969cfa4142ec8e8f2f136610e
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-07-21 22:54:31 +0900

    create project

---
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/HospitalHereApplication.java
@@ -0,0 +1,13 @@
+package com.team5.hospital_here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalHereApplication.class, args);
+	}
+
+}
