22fd16dcfbbb3810ac7a11d5437880937b7c5333
sanghyuk <ish775800@gmail.com>
2024-07-23 13:33:59 +0900

    fix: ErrorCode code값 수정

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/exception/ErrorCode.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/exception/ErrorCode.java
@@ -12,9 +12,9 @@ public enum ErrorCode {
     NO_PERMISSION(400,"NO_PERMISSION","권한없음"),
     INVALID_USER_CREDENTIALS(401, "INVALID_USER_CREDENTIALS", "비밀번호 틀림"),
     CHAT_ROOM_NOT_FOUND(404, "CHAT_ROOM_NOT_FOUND", "존재하지 않는 채팅방입니다"),
-    CHAT_ROOM_ACCESS_FAILED(404, "CHAT_ROOM_ACCESS_FAILED", "채팅방에 접근할 수 없습니다"),
-    CHAT_ROOM_ALREADY_BELONG(404, "CHAT_ROOM_ALREADY_BELONG", "이미 참여중인 채팅방입니다"),
-    CHAT_ROOM_NOT_BELONG(404, "CHAT_ROOM_NOT_BELONG", "속해있지 않은 채팅방입니다");
+    CHAT_ROOM_ACCESS_FAILED(400, "CHAT_ROOM_ACCESS_FAILED", "채팅방에 접근할 수 없습니다"),
+    CHAT_ROOM_ALREADY_BELONG(400, "CHAT_ROOM_ALREADY_BELONG", "이미 참여중인 채팅방입니다"),
+    CHAT_ROOM_NOT_BELONG(400, "CHAT_ROOM_NOT_BELONG", "속해있지 않은 채팅방입니다");
 
     //NOTE: HttpStatus code
     public int code;
