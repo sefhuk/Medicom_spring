10569275c423e845f05fc0bb240faeda607d4aa2
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-07-23 12:51:17 +0900

    fix: JwtAuthenticationFilter

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/jwt/CustomeUser.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/jwt/CustomeUser.java
@@ -2,9 +2,11 @@ package com.team5.hospital_here.common.jwt;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+@Getter
 public class CustomeUser extends User {
 
     private com.team5.hospital_here.user.entity.User user;
--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/jwt/JwtAuthenticationFilter.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/jwt/JwtAuthenticationFilter.java
@@ -4,9 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,7 +39,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             CustomeUser user = (CustomeUser) userDetailsService.loadUserByUsername(email);
 
             UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                user, null, user.getAuthorities());
+                user, null, List.of(new SimpleGrantedAuthority(user.getUser().getRole().name())));
+
+//            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+//                user, null, user.getAuthorities());
 
             authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authentication);
