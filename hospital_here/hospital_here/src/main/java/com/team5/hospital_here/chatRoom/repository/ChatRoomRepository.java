75ff9270a410abc74071ac2d953c564eba514a7f
sanghyuk <ish775800@gmail.com>
2024-07-23 23:00:00 +0900

    fix: 채팅 목록 조회 JPQL 문제, N+1 문제 해결

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/chatRoom/repository/ChatRoomRepository.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/chatRoom/repository/ChatRoomRepository.java
@@ -2,6 +2,7 @@ package com.team5.hospital_here.chatRoom.repository;
 
 import com.team5.hospital_here.chatRoom.entity.ChatRoom;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,11 +11,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-    @Query("SELECT cr FROM ChatRoom cr JOIN FETCH cr.chatMessages"
-        + " JOIN FETCH cr.user1"
-        + " JOIN FETCH cr.user2"
-        + " WHERE (cr.user1.id = :id OR cr.user2.id = :id)"
-        + " AND cr.leaveUser.id != :id"
+    @EntityGraph(attributePaths = {"chatMessages", "user1", "user2", "leaveUser"})
+    @Query("SELECT cr FROM ChatRoom cr"
+        + " WHERE ( cr.user1.id = :id AND cr.user2.id = :id )"
+        + " OR ( cr.leaveUser.id != :id OR cr.leaveUser.id IS NULL ) "
         + " ORDER BY cr.createdAt")
     List<ChatRoom> findAllWithUserId(@Param("id") Long id);
 }
