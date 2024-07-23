ec92ca87b075ed1e452a4dc2b4857bc5a9b2b448
sanghyuk <ish775800@gmail.com>
2024-07-23 11:58:39 +0900

    feat: chatRoom 관련 ErrorCode enum

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/exception/ErrorCode.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/exception/ErrorCode.java
@@ -1,15 +1,18 @@
 package com.team5.hospital_here.common.exception;
 
-
+import lombok.Builder;
 import lombok.Getter;
 
+@Getter
 public enum ErrorCode {
-    ;
+    CHAT_ROOM_NOT_FOUND(404, "NOT_FOUND", "존재하지 않는 채팅방입니다"),
+    CHAT_ROOM_ACCESS_FAILED(404, "BAD_REQUEST", "채팅방에 접근할 수 없습니다");
 
     public int code;
     public String codeName;
     public String message;
 
+    @Builder
     ErrorCode(int code, String codeName, String message) {
         this.code = code;
         this.codeName = codeName;
