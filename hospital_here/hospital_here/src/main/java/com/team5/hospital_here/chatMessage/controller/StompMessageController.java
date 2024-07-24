92b72ccd41acef0cecfa78f91c4820b2c190a53f
sanghyuk <ish775800@gmail.com>
2024-07-24 10:27:59 +0900

    fix: STOMP 메시지 핸들러 경로변수 인식 문제 해결

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/chatMessage/controller/StompMessageController.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/chatMessage/controller/StompMessageController.java
@@ -5,6 +5,7 @@ import com.team5.hospital_here.chatMessage.dto.ChatMessageResponseDTO;
 import com.team5.hospital_here.chatMessage.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ public class StompMessageController {
     private final ChatMessageService chatMessageService;
 
     @MessageMapping("/chat/{chatRoomId}")
-    public ResponseEntity<Void> messageHandler(@PathVariable Long chatRoomId, @RequestBody
+    public ResponseEntity<Void> messageHandler(@DestinationVariable Long chatRoomId, @RequestBody
     ChatMessageRequestDTO chatMessageRequestDTO) {
         ChatMessageResponseDTO chatMessage = chatMessageService.addChatMessage(chatRoomId,
             chatMessageRequestDTO);
