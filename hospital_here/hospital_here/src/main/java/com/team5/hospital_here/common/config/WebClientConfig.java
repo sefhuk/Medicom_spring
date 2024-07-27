f3fa054e7f92ce0ce98c9cf3d8ed8fb16a35d018
narim <ehdrkd648@naver.com>
2024-07-27 18:46:56 +0900

    chore: add webflux dependency, feat: add AI logic

---
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/config/WebClientConfig.java
@@ -0,0 +1,13 @@
+package com.team5.hospital_here.common.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
