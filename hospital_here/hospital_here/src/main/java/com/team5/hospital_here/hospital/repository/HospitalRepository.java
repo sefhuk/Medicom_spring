572f8c27794c3258184a8da3ed8a928d1ee4680e
[CLOUD3] 허율 <yul3758@naver.com>
2024-08-05 11:49:29 +0900

    fix : 매핑 안되어 있는 departmentName data도 추가

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/hospital/repository/HospitalRepository.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/hospital/repository/HospitalRepository.java
@@ -6,6 +6,8 @@ import com.team5.hospital_here.hospital.entity.Hospital;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +15,7 @@ import java.util.List;
 @Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 
-    @Query("SELECT h FROM Hospital h JOIN h.hospitalDepartments hd JOIN hd.department d WHERE "
+    @Query("SELECT h FROM Hospital h LEFT JOIN h.hospitalDepartments hd LEFT JOIN hd.department d WHERE "
             + "(:name IS NULL OR h.name LIKE %:name%) AND "
             + "(:address IS NULL OR h.address LIKE %:address%) AND "
             + "(:departmentName IS NULL OR :departmentName = '' OR d.name LIKE %:departmentName%)")
