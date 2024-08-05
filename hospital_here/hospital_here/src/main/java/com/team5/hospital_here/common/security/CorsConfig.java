62f6eaa1dacd44fbde4a8e27d91ed77b4b1c213f
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-08-05 19:08:29 +0900

    fix: cors 허용 ip 수정

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/security/CorsConfig.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         //TODO: 배포시 Origins 추가 필요
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://34.64.103.17:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
