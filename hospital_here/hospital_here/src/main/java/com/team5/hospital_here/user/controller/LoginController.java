8f81943a861ec7f67b3a0a1bfaf675d778fed4bb
[Cloud3] 전일우 <59927358+FFFace@users.noreply.github.com>
2024-08-06 10:58:21 +0900

    cicd test

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/user/controller/LoginController.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/user/controller/LoginController.java
@@ -16,10 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
-
-//cicd 테스트
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
