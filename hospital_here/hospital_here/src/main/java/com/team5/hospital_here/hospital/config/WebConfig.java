f7a0c10e0d41dd71df28c3a5807b7785af062b00
narim <ehdrkd648@naver.com>
2024-08-09 00:38:15 +0900

    fix: webconfig domain 추가

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/hospital/config/WebConfig.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/hospital/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://34.64.103.17") // 허용할 프론트엔드 도메인
+                .allowedOrigins("http://localhost:3000", "http://34.64.103.17", "https://34.64.103.17", "https://localhost:3000") // 허용할 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
