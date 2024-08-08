84c7fb637a8ef1dbd123dcb3803d7fba20293a2a
[Cloud3] 전일우 <ilwoo6422@naver.com>
2024-08-08 15:15:22 +0000

    Update CorsConfig.java

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/security/CorsConfig.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/common/security/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(
-            List.of("http://34.64.103.17", "https://34.64.103.17", "http://localhost:3000"));
+            List.of("http://34.64.103.17", "https://34.64.103.17", 
+                        "https://kdt-cloud-3-team05-final.elicecoding.com", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
