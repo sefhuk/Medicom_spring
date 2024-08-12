c071739a94a4fc74264e74a5822ca204f38e89b0
[Cloud3] 허율 <yul3758@naver.com>
2024-08-12 04:44:49 +0000

    fix : api 엔드포인트 추가

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/review/controller/ReviewController.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/review/controller/ReviewController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/review")
+@RequestMapping("/api/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
