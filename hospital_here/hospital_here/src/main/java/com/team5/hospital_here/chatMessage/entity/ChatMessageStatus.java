854d5218f66ea1c7b3ace896fef5a0ae625c6a35
sanghyuk <ish775800@gmail.com>
2024-08-12 15:39:52 +0900

    feat: 채팅 메시지 상태 엔티티 구현 및 연관관계 조정

---
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/chatMessage/entity/ChatMessageStatus.java
@@ -0,0 +1,37 @@
+package com.team5.hospital_here.chatMessage.entity;
+
+import com.team5.hospital_here.common.baseEntity.BaseEntity;
+import com.team5.hospital_here.user.entity.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChatMessageStatus extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_message_id")
+    private ChatMessage chatMessage;
+
+    private Boolean isRead;
+}
