8d6f50b751683730f8732b1114863c0c13a3702c
[Cloud3] 허율 <yul3758@naver.com>
2024-08-12 06:43:31 +0000

    Update ReviewController.java

--- hospital_here/hospital_here/src/main/java/com/team5/hospital_here/review/controller/ReviewController.java
+++ hospital_here/hospital_here/src/main/java/com/team5/hospital_here/review/controller/ReviewController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/review")
+@RequestMapping("/review")
 public class ReviewController {
 
     private final ReviewService reviewService;
